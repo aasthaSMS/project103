81bea070c90c9be66cfe9a9d6c20e6ec6542bfa5
aasthaSMS <108734036+aasthaSMS@users.noreply.github.com>
2023-12-26 11:22:52 +0530

    Add files via upload

---
+++ organize103.py
@@ -0,0 +1,75 @@
+import sys
+import time
+import random
+
+import os
+import shutil
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+from_dir = "C:/Users/LENOVO/Downloads"
+to_dir = "C:/Users/LENOVO/Desktop/download_files"
+
+dir_tree = {
+    "Image_Files": ['.jpg', '.jpeg', '.png', '.gif', '.jfif'],
+    "Video_Files": ['.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.mp4', '.m4p', '.m4v', '.avi', '.mov'],
+    "Document_Files": ['.ppt', '.xls', '.csv', '.pdf', '.txt'],
+    "Setup_Files": ['.exe', '.bin', '.cmd', '.msi', '.dmg'],
+    "Pdf_Files": ['.pdf'],
+    "Ppt_Files": ['.pptx'],
+    "Excel_Files": ['.xlsx'],
+    "Python_files": ['.py']
+}
+
+# Event Hanlder Class
+
+class FileMovementHandler(FileSystemEventHandler):
+
+    def on_created(self, event):
+        name,extention = os.path.splitext(event.src_path)
+        time.sleep(1)
+        for key,value in dir_tree.items():
+            if extention in value:
+                file_name = os.path.basename(event.src_path)
+                path1 = from_dir+"/"+file_name
+                path2 = to_dir+"/"+key
+                path3 = to_dir+"/"+key+"/"+file_name
+                if os.path.exists(path2):
+                    print("moving.."+file_name+"...")
+                    shutil.move(path1,path3)
+                else:
+                    os.makedirs(path2)
+                    print("moving.."+file_name+"...")
+                    shutil.move(path1,path3)
+
+        print(event)
+        print(event.src_path)
+
+
+# Initialize Event Handler Class
+event_handler = FileMovementHandler()
+
+
+# Initialize Observer
+observer = Observer()
+
+# Schedule the Observer
+observer.schedule(event_handler, from_dir, recursive=True)
+
+
+# Start the Observer
+observer.start()
+
+try:
+    while True:
+        time.sleep(2)
+        print("running...")
+except KeyboardInterrupt:
+    print("stopped")
+    observer.stopped()
+
+
+
+    
